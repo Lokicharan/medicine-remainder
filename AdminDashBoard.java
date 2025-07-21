51d32a1fbc2b9009418314d527dee889aa02a928
S Roshan <shaikroshan.656@gmail.com>
2025-07-21 12:23:04 +0530

    Update AdminDashBoard.java
    testing the github code once

--- AdminDashBoard.java
+++ AdminDashBoard.java
@@ -4,7 +4,11 @@ public class AdminDashBoard {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+for(int i=0;i<5;i++)
+	{
+		System.out.println(i);
 	}
+	}
+	
 
 }
